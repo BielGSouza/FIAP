bc391617b75ca6635897d122aff90d076a60cdec
Gabriel de Souza <gabrielsouzaca05@gmail.com>
2026-03-04 10:59:15 -0300

    Estudos de algoritimos em Python
    Começo e execução do estudo de algoritmos e logica de programação em Python, desafios como somar fazem parte desse inicio.

--- 1º ANO 2026/Fases/Fase 01/Capitulos/Capitulo 06/index.py
+++ 1º ANO 2026/Fases/Fase 01/Capitulos/Capitulo 06/index.py
@@ -1,7 +1,4 @@
-print("Esse programa inverte os conteúdos de duas variáveis")
-A = input("Digite o conteúdo da variável 1: ")
-B = input("Digite o conteúdo da variável 2: ")
-troca = A
-A = B
-B = troca
-print("Agora que trocamos, a variável A contém {} e a variável B contém {}".format(A, B))
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+soma = n1 + n2
+print("Soma", soma)
